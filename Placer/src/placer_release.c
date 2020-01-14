d78963d8aca09117aa2886531e84621567e35156
Chip Overclock <coverclock@diag.com>
2020-01-14 13:55:03 -0700

    [1] Successful first build.

---
+++ Placer/src/placer_release.c
@@ -0,0 +1,18 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2020 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock (coverclock@diag.com)<BR>
+ * http://www.diag.com/navigation/downloads/Placer.html<BR>
+ * The purpose of this translation unit is to embed the release string
+ * inside the library or shared object. The object module will be statically
+ * linked into an application only if the translation unit makes explicit
+ * references to the storage here as external references.
+ */
+
+#include "com/diag/placer/placer_release.h"
+
+const char COM_DIAG_PLACER_RELEASE_KEYWORD[] = "COM_DIAG_PLACER_RELEASE=" COM_DIAG_PLACER_RELEASE;
+const char * COM_DIAG_PLACER_RELEASE_VALUE = &COM_DIAG_PLACER_RELEASE_KEYWORD[sizeof("COM_DIAG_PLACER_RELEASE=") - 1];
---
+++ Placer/src/placer_vintage.c
@@ -0,0 +1,18 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2020 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock (coverclock@diag.com)<BR>
+ * http://www.diag.com/navigation/downloads/Placer.html<BR>
+ * The purpose of this translation unit is to embed the vintage string
+ * inside the library or shared object. The object module will be statically
+ * linked into an application only if the translation unit makes explicit
+ * references to the storage here as external references.
+ */
+
+#include "com/diag/placer/placer_vintage.h"
+
+const char COM_DIAG_PLACER_VINTAGE_KEYWORD[] = "COM_DIAG_PLACER_VINTAGE=" COM_DIAG_PLACER_VINTAGE;
+const char * COM_DIAG_PLACER_VINTAGE_VALUE = &COM_DIAG_PLACER_VINTAGE_KEYWORD[sizeof("COM_DIAG_PLACER_VINTAGE=") - 1];
