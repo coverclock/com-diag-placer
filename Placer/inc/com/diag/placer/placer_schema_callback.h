7eb724eb837db55e615f34f1a8e6b3e04ba79950
Chip Overclock <coverclock@diag.com>
2020-02-13 12:51:58 -0700

    [1] Schema callback unit test PASSES.

--- Placer/inc/com/diag/placer/placer_schema_callback.h
+++ Placer/inc/com/diag/placer/placer_schema_callback.h
@@ -12,43 +12,62 @@
 
 #include "placer_schema_undef.h"
 
+/*
+ * The prototype for this function matches that of an SQLite3 callback
+ * function, so that it can be used directly as a callback, or just called
+ * from within a callback. Casting the void pointer to a triple indirect
+ * variable makes even my brain hurt. The void pointer points to a variable
+ * whose value is a pointer to an array that contains pointers to the
+ * structure. So the first dereference gets to the variable that points to
+ * the array; incrementing that variable indexs through the array. The
+ * second dereference accesses a pointer to a structure. The list of pointers
+ * to structures in the awway is terminated by a null structure pointer.
+ * This allows this callback to be used in an operation that selects rows
+ * from a table table, import the  columns from each row, and populate
+ * several structures. The generated function is paranoid: it not only
+ * checks for the array not having enough entries, but for the number of
+ * of columns not matching, for the names of the columns not matching,
+ * and for the data type imports failing.
+ */
+
 #define PLACER_SCHEMA(_STRUCTURE_) \
 int placer_##_STRUCTURE_##_callback(void * vp, int ncols, char ** value, char ** keyword) { \
     int rc = SQLITE_ERROR; \
-    struct _STRUCTURE_ ** pp = (struct _STRUCTURE_ **)0; \
+    struct _STRUCTURE_ *** pp = (struct _STRUCTURE_ ***)0; \
     int ii = 0; \
-    pp = (struct _STRUCTURE_ **)vp; \
+    pp = (struct _STRUCTURE_ ***)vp; \
     do { \
-        if (*pp == (struct _STRUCTURE_ *)0) { break; }
+        if (**pp == (struct _STRUCTURE_ *)0) { break; }
 
 #define PLACER_BLOB(_NAME_, _ITEMS_) \
         if (ii >= ncols) { break; } \
         if (strcmp(keyword[ii], #_NAME_) != 0) { break; } \
-        if (placer_BLOB_import((*pp)->_NAME_, _ITEMS_, value[ii++]) != SQLITE_OK) { break; }
+        if (placer_BLOB_import((**pp)->_NAME_, _ITEMS_, value[ii++]) != SQLITE_OK) { break; }
 
 #define PLACER_FLOAT(_NAME_) \
         if (ii >= ncols) { break; } \
-        if (placer_FLOAT_import(&((*pp)->_NAME_), value[ii++]) != SQLITE_OK) { break; }
+        if (strcmp(keyword[ii], #_NAME_) != 0) { break; } \
+        if (placer_FLOAT_import(&((**pp)->_NAME_), value[ii++]) != SQLITE_OK) { break; }
 
 #define PLACER_INTEGER(_NAME_) \
         if (ii >= ncols) { break; } \
         if (strcmp(keyword[ii], #_NAME_) != 0) { break; } \
-        if (placer_INTEGER_import(&((*pp)->_NAME_), value[ii++]) != SQLITE_OK) { break; }
+        if (placer_INTEGER_import(&((**pp)->_NAME_), value[ii++]) != SQLITE_OK) { break; }
 
 #define PLACER_INTEGER64(_NAME_) \
         if (ii >= ncols) { break; } \
         if (strcmp(keyword[ii], #_NAME_) != 0) { break; } \
-        if (placer_INTEGER64_import(&((*pp)->_NAME_), value[ii++]) != SQLITE_OK) { break; }
+        if (placer_INTEGER64_import(&((**pp)->_NAME_), value[ii++]) != SQLITE_OK) { break; }
 
 #define PLACER_TEXT(_NAME_, _ITEMS_) \
         if (ii >= ncols) { break; } \
         if (strcmp(keyword[ii], #_NAME_) != 0) { break; } \
-        if (placer_TEXT_import((*pp)->_NAME_, _ITEMS_, value[ii++]) != SQLITE_OK) { break; }
+        if (placer_TEXT_import((**pp)->_NAME_, _ITEMS_, value[ii++]) != SQLITE_OK) { break; }
 
 #define PLACER_TEXT16(_NAME_, _ITEMS_) \
         if (ii >= ncols) { break; } \
         if (strcmp(keyword[ii], #_NAME_) != 0) { break; } \
-        if (placer_TEXT16_import((*pp)->_NAME_, _ITEMS_, value[ii++]) != SQLITE_OK) { break; }
+        if (placer_TEXT16_import((**pp)->_NAME_, _ITEMS_, value[ii++]) != SQLITE_OK) { break; }
 
 #define PLACER_FIELD(_CONSTRAINTS_)
 
--- Placer/tst/unittest-schema.c
+++ Placer/tst/unittest-schema.c
@@ -120,12 +120,67 @@ int main(void)
 
     {
         placer_BLOB_t result[8]  = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, };
-        char one[8]  = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, };
-        char two[9]  = { 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0xa5, };
-        EXPECT(placer_BLOB_import(result, sizeof(result), one) == SQLITE_OK);
-        EXPECT(memcmp(result, one, sizeof(result)) == 0);
-        EXPECT(placer_BLOB_import(result, sizeof(result), two) == SQLITE_OK);
-        EXPECT(memcmp(result, two, sizeof(result)) == 0);
+        char value[2][8] = {
+            { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, },
+            { 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, },
+        };
+        EXPECT(placer_BLOB_import(result, sizeof(result), value[0]) == SQLITE_OK);
+        EXPECT(memcmp(result, value[0], sizeof(result)) == 0);
+        EXPECT(placer_BLOB_import(result, sizeof(result), value[1]) == SQLITE_OK);
+        EXPECT(memcmp(result, value[1], sizeof(result)) == 0);
+    }
+
+    {
+        struct UnitTestSchema results[3] = { { 0, } };
+        struct UnitTestSchema * gather[] = { &(results[0]), &(results[1]), &(results[2]), (struct UnitTestSchema *)0 };
+        struct UnitTestSchema ** here = &(gather[0]);
+        placer_BLOB_t image[4][4096] = { { 0x11, }, { 0x22, }, { 0x33, }, { 0x44 }, };
+        char * value[4][6] = {
+            { "1", (char *)L"Chip Overclock", "63.625", (char *)&(image[0]), "42", "123456789", },
+            { "2", (char *)L"Red Black", "51.", (char *)&(image[1]), "86", "234567890", },
+            { "3", (char *)L"The French Woman", "62", (char *)&(image[2]), "99", "345678901", },
+            { "4", (char *)L"Dread Pirate Roberts", "31", (char *)&(image[3]), "1", "456789012", },
+        };
+        char * keyword[] = { "id", "name", "age", "image", "sn", "ssn", };
+        int rc;
+
+        COMMENT("1");        
+        rc = placer_UnitTestSchema_callback((void *)&here, 6, value[0], keyword);
+        ASSERT(rc == SQLITE_OK);
+        EXPECT(here = &(gather[1]));
+        EXPECT(results[0].id == (int32_t)1);
+        EXPECT(wcscmp(results[0].name,  L"Chip Overclock") == 0);
+        EXPECT(results[0].age == (double)63.625);
+        EXPECT(results[0].image[0] == (uint8_t)0x11);
+        EXPECT(results[0].sn == (int64_t)42);
+        EXPECT(strcmp(results[0].ssn, "123456789") == 0);
+
+        COMMENT("2");        
+        rc = placer_UnitTestSchema_callback((void *)&here, 6, value[1], keyword);
+        ASSERT(rc == SQLITE_OK);
+        EXPECT(here = &(gather[2]));
+        EXPECT(results[1].id == (int32_t)2);
+        EXPECT(wcscmp(results[1].name,  L"Red Black") == 0);
+        EXPECT(results[1].age == (double)51.0);
+        EXPECT(results[1].image[0] == (uint8_t)0x22);
+        EXPECT(results[1].sn == (int64_t)86);
+        EXPECT(strcmp(results[1].ssn, "234567890") == 0);
+
+        COMMENT("3");        
+        rc = placer_UnitTestSchema_callback((void *)&here, 6, value[2], keyword);
+        ASSERT(rc == SQLITE_OK);
+        EXPECT(here = &(gather[3]));
+        EXPECT(results[2].id == (int32_t)3);
+        EXPECT(wcscmp(results[2].name,  L"The French Woman") == 0);
+        EXPECT(results[2].age == (double)62);
+        EXPECT(results[2].image[0] == (uint8_t)0x33);
+        EXPECT(results[2].sn == (int64_t)99);
+        EXPECT(strcmp(results[2].ssn, "345678901") == 0);
+
+        COMMENT("4");        
+        rc = placer_UnitTestSchema_callback((void *)&here, 6, value[3], keyword);
+        ASSERT(rc == SQLITE_ERROR);
+        EXPECT(here = &(gather[3]));
     }
 
     EXIT();
