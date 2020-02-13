3d2a1664980232cdbdab2246f4048dc2d9f59911
Chip Overclock <coverclock@diag.com>
2020-02-13 10:22:26 -0700

    [1] Import unit tests PASS.

--- Placer/tst/unittest-schema.c
+++ Placer/tst/unittest-schema.c
@@ -118,5 +118,15 @@ int main(void)
         EXPECT(wcscmp(result, L"JKLMNOPQ") == 0);
     }
 
+    {
+        placer_BLOB_t result[8]  = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, };
+        char one[8]  = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, };
+        char two[9]  = { 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0xa5, };
+        EXPECT(placer_BLOB_import(result, sizeof(result), one) == SQLITE_OK);
+        EXPECT(memcmp(result, one, sizeof(result)) == 0);
+        EXPECT(placer_BLOB_import(result, sizeof(result), two) == SQLITE_OK);
+        EXPECT(memcmp(result, two, sizeof(result)) == 0);
+    }
+
     EXIT();
 }
