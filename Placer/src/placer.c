f3c1b4eb6e1a8404e533e1dc5ed102a5a0ec0338
Chip Overclock <coverclock@diag.com>
2020-02-14 09:52:55 -0700

    [1] Merged placer_schema into placer. [2] BUILDS. [3] Schema unit test PASSES.

--- Placer/src/placer.c
+++ Placer/src/placer.c
@@ -8,6 +8,7 @@
  * https://github.com/coverclock/com-diag-placer<BR>
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -269,3 +270,87 @@ int placer_db_exec(sqlite3 * db, const char * sql, int (*cp)(void *, int, char *
 
     return rc;
 }
+
+/*******************************************************************************
+ *
+ ******************************************************************************/
+
+int placer_BLOB_import(placer_BLOB_t * dest, size_t items, const char * src)
+{
+    int rc = SQLITE_OK;
+
+    (void)memcpy(dest, (placer_BLOB_t *)src, items);
+
+    return rc;
+}
+
+int placer_FLOAT_import(placer_FLOAT_t * dest, const char * src)
+{
+    int rc = SQLITE_ERROR;
+    char * end = (char *)0;
+
+    *dest = strtod(src, &end);
+    if ((end != (char *)0) && (*end == '\0')) {
+        rc = SQLITE_OK;
+    }
+
+    return rc;
+}
+
+int placer_INTEGER_import(placer_INTEGER_t * dest, const char * src)
+{
+    int rc = SQLITE_ERROR;
+    char * end = (char *)0;
+
+    *dest = strtol(src, &end, 10);
+    if ((end != (char *)0) && (*end == '\0')) {
+        rc = SQLITE_OK;
+    }
+
+    return rc;
+}
+
+int placer_INTEGER64_import(placer_INTEGER64_t * dest, const char * src)
+{
+    int rc = SQLITE_ERROR;
+    char * end = (char *)0;
+
+    *dest = strtoll(src, &end, 10);
+    if ((end != (char *)0) && (*end == '\0')) {
+        rc = SQLITE_OK;
+    }
+
+    return rc;
+}
+
+int placer_TEXT_import(placer_TEXT_t * dest, size_t items, const char * src)
+{
+    int rc = SQLITE_ERROR;
+
+    (void)strncpy(dest, (placer_TEXT_t *)src, items);
+    if (dest[items - 1] == '\0') {
+        rc = SQLITE_OK;
+    } else {
+        dest[items - 1] = '\0';
+    }
+
+    return rc;
+}
+
+int placer_TEXT16_import(placer_TEXT16_t * dest, size_t items, const char * src)
+{
+    int rc = SQLITE_ERROR;
+
+    (void)wcsncpy(dest, (placer_TEXT16_t *)src, items);
+    if (dest[items - 1] == L'\0') {
+        rc = SQLITE_OK;
+    } else {
+        dest[items - 1] = L'\0';
+    }
+
+    return rc;
+}
+
+/*******************************************************************************
+ *
+ ******************************************************************************/
