a7be7d0aaab7f297b6dc76bae22e594badeb688b
Chip Overclock <coverclock@diag.com>
2020-03-06 13:15:06 -0700

    [1] Added census and survey test suite scripts. [2] TESTED.

---
+++ Placer/fun/census-suite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+OUT=./out/host
+LOG=${OUT}/log
+${OUT}/bin/setup
+mkdir -p ${LOG}
+(
+    rm -f out/host/sql/census.db
+    cp /dev/null ${LOG}/CENSUS_TEST_FILE
+    census -D out/host/sql/census.db -c -i -v .
+    census -D out/host/sql/census.db -v -5
+    rm -f ${LOG}/CENSUS_TEST_FILE
+    census -D out/host/sql/census.db -r -v .
+    census -D out/host/sql/census.db -v -6
+    census -D out/host/sql/census.db -v -0
+) 2>&1 | tee ${LOG}/census.log
