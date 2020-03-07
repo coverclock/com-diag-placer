9909f6de364af9dbc13f83a23cb3a7f340fdf895
Chip Overclock <coverclock@diag.com>
2020-03-07 16:15:24 -0700

    [1] Fixed (again) census, survey suite scripts (TESTED).

--- Placer/fun/census-suite.sh
+++ Placer/fun/census-suite.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-OUT=./out/host
-LOG=${OUT}/log
-. ${OUT}/bin/setup
-mkdir -p ${LOG}
-(
-    rm -f out/host/sql/census.db
-    cp /dev/null ${LOG}/CENSUS_TEST_FILE
-    census -D out/host/sql/census.db -c -i -v .
-    census -D out/host/sql/census.db -v -5
-    rm -f ${LOG}/CENSUS_TEST_FILE
-    census -D out/host/sql/census.db -r -v .
-    census -D out/host/sql/census.db -v -6
-    census -D out/host/sql/census.db -v -0
-) 2>&1 | tee ${LOG}/census.log
+# Copyright 2020 Digital Aggregates Corporation, Arvada CO USA.
+# Author: coverclock@diag.com
+OUT=./$(make out)
+mkdir -p ${OUT}/tmp
+rm -f ${OUT}/sql/census.db
+cp /dev/null ${OUT}/tmp/CENSUS_TEST_FILE
+census -D ${OUT}/sql/census.db -c -i -v .
+census -D ${OUT}/sql/census.db -v -5
+rm -f ${OUT}/tmp/CENSUS_TEST_FILE
+census -D ${OUT}/sql/census.db -r -v .
+census -D ${OUT}/sql/census.db -v -6
+census -D ${OUT}/sql/census.db -v -0
--- Placer/fun/survey-suite.sh
+++ Placer/fun/survey-suite.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
-OUT=./out/host
-LOG=${OUT}/log
-. ${OUT}/bin/setup
-mkdir -p ${LOG}
-(
-    rm -f out/host/sql/survey.db
-    cp /dev/null ${LOG}/SURVEY_TEST_FILE_1
-    cp /dev/null ${LOG}/SURVEY_TEST_FILE_2
-    ln ${LOG}/SURVEY_TEST_FILE_2 ${LOG}/SURVEY_TEST_FILE_3
-    ln ${LOG}/SURVEY_TEST_FILE_2 ${LOG}/SURVEY_TEST_FILE_4
-    ln ${LOG}/SURVEY_TEST_FILE_2 ${LOG}/SURVEY_TEST_FILE_5
-    survey -D out/host/sql/survey.db -c -i -v .
-    survey -D out/host/sql/survey.db -v -5
-    rm ${LOG}/SURVEY_TEST_FILE_1
-    survey -D out/host/sql/survey.db -r -v .
-    survey -D out/host/sql/survey.db -v -6
-    survey -D out/host/sql/survey.db -v -0
-    survey -D out/host/sql/survey.db -7 -P $(realpath ${LOG}/SURVEY_TEST_FILE_2)
-    survey -D out/host/sql/survey.db -7 -P $(realpath ${LOG}/SURVEY_TEST_FILE_3)
-    survey -D out/host/sql/survey.db -7 -P $(realpath ${LOG}/SURVEY_TEST_FILE_4)
-    survey -D out/host/sql/survey.db -7 -P $(realpath ${LOG}/SURVEY_TEST_FILE_5)
-    survey -D out/host/sql/survey.db -8 -I $(ls -i ${LOG}/SURVEY_TEST_FILE_2 | cut -d ' ' -f 1)
-    rm ${LOG}/SURVEY_TEST_FILE_2
-    rm ${LOG}/SURVEY_TEST_FILE_3
-    rm ${LOG}/SURVEY_TEST_FILE_4
-    rm ${LOG}/SURVEY_TEST_FILE_5
-) 2>&1 | tee ${LOG}/survey.log
+# Copyright 2020 Digital Aggregates Corporation, Arvada CO USA.
+# Author: coverclock@diag.com
+OUT=./$(make out)
+mkdir -p ${OUT}/tmp
+rm -f ${OUT}/sql/survey.db
+cp /dev/null ${OUT}/tmp/SURVEY_TEST_FILE_1
+cp /dev/null ${OUT}/tmp/SURVEY_TEST_FILE_2
+ln ${OUT}/tmp/SURVEY_TEST_FILE_2 ${OUT}/tmp/SURVEY_TEST_FILE_3
+ln ${OUT}/tmp/SURVEY_TEST_FILE_2 ${OUT}/tmp/SURVEY_TEST_FILE_4
+ln ${OUT}/tmp/SURVEY_TEST_FILE_2 ${OUT}/tmp/SURVEY_TEST_FILE_5
+survey -D ${OUT}/sql/survey.db -c -i -v .
+survey -D ${OUT}/sql/survey.db -v -5
+rm ${OUT}/tmp/SURVEY_TEST_FILE_1
+survey -D ${OUT}/sql/survey.db -r -v .
+survey -D ${OUT}/sql/survey.db -v -6
+survey -D ${OUT}/sql/survey.db -v -0
+survey -D ${OUT}/sql/survey.db -7 -P $(realpath ${OUT}/tmp/SURVEY_TEST_FILE_2)
+survey -D ${OUT}/sql/survey.db -7 -P $(realpath ${OUT}/tmp/SURVEY_TEST_FILE_3)
+survey -D ${OUT}/sql/survey.db -7 -P $(realpath ${OUT}/tmp/SURVEY_TEST_FILE_4)
+survey -D ${OUT}/sql/survey.db -7 -P $(realpath ${OUT}/tmp/SURVEY_TEST_FILE_5)
+survey -D ${OUT}/sql/survey.db -8 -I $(ls -i ${OUT}/tmp/SURVEY_TEST_FILE_2 | cut -d ' ' -f 1)
+rm ${OUT}/tmp/SURVEY_TEST_FILE_2
+rm ${OUT}/tmp/SURVEY_TEST_FILE_3
+rm ${OUT}/tmp/SURVEY_TEST_FILE_4
+rm ${OUT}/tmp/SURVEY_TEST_FILE_5
